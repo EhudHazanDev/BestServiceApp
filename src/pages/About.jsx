a121a85262b6c77cd75f95d51cdcf73eef342666
Ehud Hazan <ehudhazandev@gmail.com>
2023-02-07 10:47:55 +0200

    ädd about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const About = () => {
+export const About = () => {
   return (
     <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta eius, provident voluptatum cumque cupiditate modi nesciunt. Possimus totam, maiores quis saepe quasi sapiente eos! Error veritatis voluptates harum similique iste.</div>
   )
 }
 
-export default About
+ 
